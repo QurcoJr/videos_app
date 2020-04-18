5e2ef6f40c7a8e3de7e0c6a36f79b2e4885e0d72
QurcoJr <60933748+QurcoJr@users.noreply.github.com>
2020-04-18 20:25:36 +0400

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,66 @@
 import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+
+const KEY = 'AIzaSyD_HeZGnurKlOzF7eTxRPmA282QBmz2mIo';
 
 class App extends React.Component {
+    state = { videos: [], selectedVideo: null };
+
+    componentDidMount() {
+        this.onTermSubmit('nature');
+    }
+
+    onTermSubmit = async term => {
+        const response = await youtube.get('/search', {
+            params: {
+                q: term,
+                part: 'snippet',
+                type: 'video',
+                maxResults: 5,
+                key: KEY
+            }
+        });
+
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
+    }
+
+    onVideoSelect = video => {
+        this.setState({ selectedVideo: video });
+    };
+
+    renderHelper() {
+        if (this.state.videos.length === 0) {
+            return <div>Videos not found...</div>;
+        }
+        return (
+            <div>
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
     render() {
-        return <div>App</div>;
+        return (
+            <div className="ui container">
+                <SearchBar onFormSubmit={this.onTermSubmit} />
+                {this.renderHelper()}
+            </div>
+        );
     }
 }
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,7 +10,7 @@ class SearchBar extends React.Component {
     onFormSubmit = event => {
         event.preventDefault();
 
-        // TODO: we call callback from parent component
+        this.props.onFormSubmit(this.state.term);
     }
 
     render() {
